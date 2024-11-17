48263e84b40ff89940ba96ac53ec2950f458daa1
Ryuna4347 <yhw7979@naver.com>
2024-11-17 21:28:10 +0900

    feat: 로그아웃 기능 추가

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,6 +1,26 @@
+import app from "@/firebaseApp";
+import { getAuth, signOut } from "firebase/auth";
+import { useState } from "react";
+
 const Profile = () => {
+    const [pending, setPending] = useState(false);
+
+    const logout = async () => {
+        if (pending) return;
+        try {
+            setPending(true);
+            const auth = getAuth(app);
+            signOut(auth);
+            setPending(false);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setPending(false);
+        }
+    };
+
     return (
-        <div className={"container flex flex-col w-10/12 m-auto gap-3"}>
+        <div className={"container flex flex-col gap-3 m-auto w-10/12"}>
             <div className={"flex justify-between mt-4"}>
                 <div className={"flex flex-row gap-3"}>
                     <div>
@@ -11,7 +31,9 @@ const Profile = () => {
                         <span className={"text-sm text-gray-700"}>이름</span>
                     </div>
                 </div>
-                <button className="hover:underline">로그아웃</button>
+                <button className="hover:underline" onClick={logout}>
+                    로그아웃
+                </button>
             </div>
         </div>
     );
